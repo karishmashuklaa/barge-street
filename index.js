ff3a22135fb8021795bb9f17e12f6c56d0648b4a
karishmas6 <carishmashukla28@gmail.com>
2021-06-14 18:04:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const score = document.querySelector('.score');
+const startScreen = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
+
+// console.log(gameArea);
+
+startScreen.addEventListener('click', start);
+
+let keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+}
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+function keyDown(e){
+    e.preventDefault();
+    keys[e.key] = true;
+    console.log(keys);
+}
+
+function keyUp(e){
+    e.preventDefault();
+    keys[e.key] = false;
+    console.log(keys);
+}
